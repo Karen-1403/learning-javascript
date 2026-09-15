8464eb51c9d06fea2cd5ea76a6c4526b0afb3903
Karen-1403 <karenmourice@gmail.com>
2026-09-15 12:47:43 +0300

    Refactoring code using modules, better naming and separation of concerns

--- javascript-amazon-project/data/cart.js
+++ javascript-amazon-project/data/cart.js
@@ -1,1 +1,17 @@
 export const cart = [];
+function addToCart(productId) {
+  let matchingItem;
+  cart.forEach((cartItem) => {
+    if (productId === cartItem.productId) {
+      matchingItem = cartItem;
+    }
+  });
+  if (matchingItem) {
+    matchingItem.productQuantity++;
+  } else {
+    cart.push({
+      productId: productId,
+      productQuantity: 1,
+    });
+  }
+}
