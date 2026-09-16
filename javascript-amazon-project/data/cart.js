9318742d6831f663a5fd440a0acd199dd1a0088d
Karen-1403 <karenmourice@gmail.com>
2026-09-16 20:03:15 +0300

    Making delivery options interactive

--- javascript-amazon-project/data/cart.js
+++ javascript-amazon-project/data/cart.js
@@ -64,3 +64,14 @@ export function updateQuantity(productId, newQuantity) {
 
   saveToCart();
 }
+
+export function updateDeliveryOption(productId, deliveryOptionId) {
+  let matchingItem;
+  cart.forEach((cartItem) => {
+    if (productId === cartItem.productId) {
+      matchingItem = cartItem;
+    }
+  });
+  matchingItem.deliveryOptionId = deliveryOptionId;
+  saveToCart();
+}
