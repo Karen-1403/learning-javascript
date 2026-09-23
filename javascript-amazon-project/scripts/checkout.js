673e28a4555f4d2aad615cf94ee323d5041b25f0
Karen-1403 <karenmourice@gmail.com>
2026-09-24 00:23:32 +0300

    learn promises and convert code with call back nesting issue to promises

--- javascript-amazon-project/scripts/checkout.js
+++ javascript-amazon-project/scripts/checkout.js
@@ -3,9 +3,49 @@ import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import { renderCheckoutHeader } from "./checkout/checkout.js";
 
 import { loadProducts } from "../data/products.js";
+import { cart } from "../data/cart-class.js";
+import { loadCart } from "../data/cart.js";
 
-loadProducts(() => {
+Promise.all([
+  new Promise((resolve) => {
+    loadProducts(() => {
+      resolve();
+    });
+  }),
+  new Promise((resolve) => {
+    loadCart(() => {
+      resolve();
+    });
+  }),
+]).then(() => {
   renderCheckoutHeader();
   renderOrderSummary();
   renderPaymentSummary();
 });
+
+/*new Promise((resolve) => {
+  loadProducts(() => {
+    resolve();
+  });
+})
+  .then(() => {
+    return new Promise((resolve) => {
+      loadCart(() => {
+        resolve();
+      });
+    });
+  })
+  .then(() => {
+    renderCheckoutHeader();
+    renderOrderSummary();
+    renderPaymentSummary();
+  });
+*/
+/*loadProducts(() => {
+    loadCart(() => {
+      renderCheckoutHeader();
+      renderOrderSummary();
+      renderPaymentSummary();
+    });
+  
+});*/
