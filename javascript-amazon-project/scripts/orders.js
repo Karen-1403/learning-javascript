ae5b4e480eac8e8ef1061a599e6bf66b05d5bf0e
Karen-1403 <karenmourice@gmail.com>
2026-09-24 22:57:20 +0300

    making orders page interactive

--- javascript-amazon-project/scripts/orders.js
+++ javascript-amazon-project/scripts/orders.js
@@ -2,17 +2,19 @@ import { orders } from "../data/orders.js";
 import { products, loadProductsFetch, getProduct } from "../data/products.js";
 import dayjs from "https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js";
 import { formatCurrency } from "./utils/money.js";
+import { cart } from "../data/cart-class.js";
 async function loadPage() {
   await loadProductsFetch();
   console.log(products[0]);
+  renderOrderPage();
+  function renderOrderPage() {
+    let ordersHTML = "";
 
-  let ordersHTML = "";
+    orders.forEach((order) => {
+      const orderDate = dayjs(order.orderTime);
+      const orderDateString = orderDate.format("MMMM D");
 
-  orders.forEach((order) => {
-    const orderDate = dayjs(order.orderTime);
-    const orderDateString = orderDate.format("MMMM D");
-
-    ordersHTML += `
+      ordersHTML += `
 <div class="order-container">
           <div class="order-header">
             <div class="order-header-left-section">
@@ -37,16 +39,44 @@ async function loadPage() {
           </div>
         </div>
 `;
+    });
     document.querySelector(".js-orders-grid").innerHTML = ordersHTML;
+    document.querySelectorAll(".js-buy-again-button").forEach((button) => {
+      button.addEventListener("click", (event) => {
+        const productId = event.currentTarget.dataset.productId;
+        cart.addToCart(productId);
+        button.innerHTML = "Added";
+        setTimeout(() => {
+          button.innerHTML = `
+          <img class="buy-again-icon" src="images/icons/buy-again.png">
+          <span class="buy-again-message">Buy it again</span>
+        `;
+        }, 1000);
+        document.querySelector(".js-cart-quantity").innerHTML =
+          cart.calculateCartQuantity();
+      });
+    });
+    document.querySelectorAll(".js-track-package-button").forEach((button) => {
+      const productId = button.dataset.productId;
+      const orderId = button.dataset.orderId;
+      button.addEventListener("click", (event) => {
+        event.preventDefault();
+        window.location.href = `https://supersimple.dev/projects/amazon/tracking.html?orderId=${orderId}&cartItemId=${productId}`;
+      });
+    });
 
-    function renderOrderProducts(order) {
-      let orderGridHTML = "";
+    document.querySelector(".js-cart-quantity").innerHTML =
+      cart.calculateCartQuantity();
+  }
 
-      order.products.forEach((product) => {
-        const productItem = getProduct(product.productId);
-        const productDeliveryDate = dayjs(product.estimatedDeliveryTime);
-        const productDeliveryDateString = productDeliveryDate.format("MMMM D");
-        orderGridHTML += `
+  function renderOrderProducts(order) {
+    let orderGridHTML = "";
+
+    order.products.forEach((product) => {
+      const productItem = getProduct(product.productId);
+      const productDeliveryDate = dayjs(product.estimatedDeliveryTime);
+      const productDeliveryDateString = productDeliveryDate.format("MMMM D");
+      orderGridHTML += `
 <div class="product-image-container">
               <img src=${productItem.image} />
             </div>
@@ -57,7 +87,7 @@ async function loadPage() {
               </div>
               <div class="product-delivery-date">Arriving on: ${productDeliveryDateString}</div>
               <div class="product-quantity">Quantity: ${product.quantity}</div>
-              <button class="buy-again-button button-primary">
+              <button class="buy-again-button js-buy-again-button button-primary" data-product-id="${productItem.id}">
                 <img class="buy-again-icon" src="images/icons/buy-again.png" />
                 <span class="buy-again-message">Buy it again</span>
               </button>
@@ -65,14 +95,14 @@ async function loadPage() {
 
             <div class="product-actions">
               <a href="tracking.html">
-                <button class="track-package-button button-secondary">
+                <button data-product-id="${productItem.id}" data-order-id="${order.id}" class="track-package-button js-track-package-button button-secondary">
                   Track package
                 </button>
               </a>
             </div>`;
-      });
-      return orderGridHTML;
-    }
-  });
+    });
+
+    return orderGridHTML;
+  }
 }
 loadPage();
